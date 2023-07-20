c0c223f84f75da9c267688a994a21e87005ec1ed
“Ronit <ronit.gupta@hcl.com>
2023-07-20 12:46:27 +0530

    Updated code as per the PR comments

--- 04 Time to Practice - Component Basics/01-starting-setup/src/components/Concepts/Concepts.js
+++ 04 Time to Practice - Component Basics/01-starting-setup/src/components/Concepts/Concepts.js
@@ -5,21 +5,13 @@ import './Concepts.css';
 const Concepts = (props) =>{
     return(
         <ul id="concepts">
-            <ConceptItems 
-                title={props.item[0].title}
-                image={props.item[0].image}
-                desc={props.item[0].description}
-            />
-            <ConceptItems 
-                title={props.item[1].title}
-                image={props.item[1].image}
-                desc={props.item[1].description}
-            />
-            <ConceptItems 
-                title={props.item[2].title}
-                image={props.item[2].image}
-                desc={props.item[2].description}
-            />
+            {props.item.map(item => (
+                <ConceptItems 
+                    title={props.title}
+                    image={props.image}
+                    desc={props.description}
+                />  
+            ))}
         </ul>
     )
 }
