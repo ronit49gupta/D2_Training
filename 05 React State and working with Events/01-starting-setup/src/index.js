5fffe6a6c235a22f003b652a70fe3d82c51e8d9a
“Ronit <ronit.gupta@hcl.com>
2023-07-24 19:21:57 +0530

    Added react-intl and update code as per the PR comments

--- 05 React State and working with Events/01-starting-setup/src/index.js
+++ 05 React State and working with Events/01-starting-setup/src/index.js
@@ -5,22 +5,20 @@ import './index.css';
 import App from './App';
 
 import {IntlProvider} from 'react-intl';
-import French from './lang/fr.json';
-import Arabic from './lang/ar.json';
 import English from './lang/en.json';
 
 const locale = navigator.language;
 
-let lang;
-if (locale==="en") {
-   lang = English;
-} else {
-   if (locale === "fr") {
-       lang = French;
-   } else {
-       lang = Arabic;
-   }
-}
+let lang = English;
+// if (locale==="en") {
+//    lang = English;
+// } else {
+//    if (locale === "fr") {
+//        lang = French;
+//    } else {
+//        lang = Arabic;
+//    }
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
