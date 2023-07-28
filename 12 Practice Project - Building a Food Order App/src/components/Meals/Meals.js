2cc61ff00418be18219a9224806fbfa8f30c7d15
“Ronit <ronit.gupta@hcl.com>
2023-07-28 12:45:01 +0530

    Completed Demo Food Order App

---
+++ 12 Practice Project - Building a Food Order App/src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummury from "./MealsSummury";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+    return (
+        <>
+            <MealsSummury/>
+            <AvailableMeals/>
+        </>
+    )
+}
+
+export default Meals;
